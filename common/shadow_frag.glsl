47dadad56f395b9a08d51f5032644d430bdc416c
SyrexSkyLine <125218537+SyrexSkyLine@users.noreply.github.com>
2025-06-19 11:31:46 +0300

    Add files via upload

--- common/shadow_frag.glsl
+++ common/shadow_frag.glsl
@@ -1,156 +1,149 @@
-/* MakeUp - shadow_frag.glsl
-Fragment shadow function.
+/* Muuf - Fragment shadow function */
 
-Javier Garduño - GNU Lesser General Public License v3.0
-*/
+float get_shadow(vec3 the_shadow_pos) {
+  float shadow_sample = 1.0;
 
-float get_shadow(vec3 the_shadow_pos, float dither) {
-    float shadow_sample = 1.0;
-
-    #if SHADOW_TYPE == 0  // Pixelated
-        shadow_sample = shadow2D(shadowtex1, the_shadow_pos).r;
-    #elif SHADOW_TYPE == 1  // Soft
-        float current_radius = dither;
-        dither *= 6.283185307179586;
-        float dither_2 = dither + 1.5707963267948966;
-
-        shadow_sample = 0.0;
-
-        vec2 offset = (vec2(cos(dither), sin(dither)) * current_radius * SHADOW_BLUR) / shadowMapResolution;
-
-        vec2 offset_2 = (vec2(cos(dither_2), sin(dither_2)) * (1.0 - current_radius) * SHADOW_BLUR) / shadowMapResolution;
-
-        float z_bias = dither * 0.00002;
-
-        shadow_sample += shadow2D(shadowtex1, vec3(the_shadow_pos.xy + offset, the_shadow_pos.z - z_bias)).r;
-        shadow_sample += shadow2D(shadowtex1, vec3(the_shadow_pos.xy - offset, the_shadow_pos.z - z_bias)).r;
-
-        shadow_sample += shadow2D(shadowtex1, vec3(the_shadow_pos.xy + offset_2, the_shadow_pos.z - z_bias)).r;
-        shadow_sample += shadow2D(shadowtex1, vec3(the_shadow_pos.xy - offset_2, the_shadow_pos.z - z_bias)).r;
-
-        // shadow_sample *= 0.5;
-        shadow_sample *= 0.25;
+  #if SHADOW_TYPE == 0  // Pixelated
+     shadow_sample = shadow2D(shadowtex1, vec3(the_shadow_pos.xy, the_shadow_pos.z - 0.001)).r;
+  #elif SHADOW_TYPE == 1  // Soft
+    #if AA_TYPE > 0
+      float dither = shifted_unit_dither(gl_FragCoord.xy);
+    #else
+      float dither = unit_dither(gl_FragCoord.xy);
     #endif
+    
+    #if defined GBUFFER_ENTITIES
+		float subsurfaceOffset = 0.00002;
+	#else
+		float subsurfaceOffset = dot(normalM.xyz, normalize(shadowLightPosition)) < 0.0 ? 0.00001 : 0.00021;
+	#endif
 
-    return shadow_sample;
+    float new_z = the_shadow_pos.z - subsurfaceOffset - (0.0000 * dither);
+
+    float current_radius = dither;
+    dither *= 6.283185307;
+
+    shadow_sample = 0.0;
+
+    vec2 offset = (vec2(cos(dither), sin(dither)) * current_radius * SHADOW_BLUR) / shadowMapResolution;
+
+    shadow_sample += shadow2D(shadowtex1, vec3(the_shadow_pos.st + offset, new_z)).r;
+    shadow_sample += shadow2D(shadowtex1, vec3(the_shadow_pos.st - offset, new_z)).r;
+
+    shadow_sample *= 0.5;
+  #endif
+
+  return clamp(shadow_sample * 2.0, 0.0, 1.0);
 }
 
-#if defined COLORED_SHADOW
+#ifdef COLORED_SHADOW
 
-    vec3 get_colored_shadow(vec3 the_shadow_pos, float dither) {
-        #if SHADOW_TYPE == 0  // Pixelated
-            float shadow_detector = 1.0;
-            float shadow_black = 1.0;
-            vec4 shadow_color = vec4(1.0);
+  vec3 get_colored_shadow(vec3 the_shadow_pos, inout vec4 block_color ) {
 
-            float alpha_complement;
+    #if SHADOW_TYPE == 0  // Pixelated
+      float shadow_detector = 1.0;
+      float shadow_black = 1.0;
+      vec4 shadow_color = vec4(1.0);
+      
+      float alpha_complement;
 
-            shadow_detector = shadow2D(shadowtex0, vec3(the_shadow_pos.xy, the_shadow_pos.z)).r;
-            if (shadow_detector < 1.0) {
-                shadow_black = shadow2D(shadowtex1, vec3(the_shadow_pos.xy, the_shadow_pos.z)).r;
-                if (shadow_black != shadow_detector) {
-                shadow_color = texture2D(shadowcolor0, the_shadow_pos.xy);
-                alpha_complement = 1.0 - shadow_color.a;
-                shadow_color.rgb = mix(shadow_color.rgb, vec3(1.0), alpha_complement);
-                shadow_color.rgb *= alpha_complement;
-                }
-            }
-            
-            shadow_color *= shadow_black;
-            shadow_color.rgb = clamp(shadow_color.rgb * (1.0 - shadow_detector) + shadow_detector, vec3(0.0), vec3(1.0));
+      shadow_detector = shadow2D(shadowtex0, vec3(the_shadow_pos.xy, the_shadow_pos.z - 0.001)).r;
+      if (shadow_detector < 1.0) {
+        shadow_black = shadow2D(shadowtex1, vec3(the_shadow_pos.xy, the_shadow_pos.z - 0.001)).r;
+        if (shadow_black != shadow_detector) {
+          shadow_color = texture2D(shadowcolor0, the_shadow_pos.xy);
+          alpha_complement = 1.0 - shadow_color.a;
+          shadow_color.rgb = mix(shadow_color.rgb, vec3(1.0), alpha_complement);
+          shadow_color.rgb *= alpha_complement;
+          #if defined GBUFFER_TERRAIN || defined GBUFFER_ENTITIES || defined GBUFFER_HAND || defined GBUUFER_HAND_WATER
+          #ifdef UNDERWATER_CAUSTIC
+		shadow_color.rgb += getCaustics(block_color) * CAUSTIC_STRENGTH;
+		#endif
+		#endif
+        }
+      }
+      
+      shadow_color *= shadow_black;
+      shadow_color.rgb = clamp(shadow_color.rgb * (1.0 - shadow_detector) + shadow_detector, vec3(0.0), vec3(1.0));
 
-            return shadow_color.rgb;
+      return shadow_color.rgb;
 
-        #elif SHADOW_TYPE == 1  // Soft
-            float shadow_detector_a = 1.0;
-            float shadow_black_a = 1.0;
-            vec4 shadow_color_a = vec4(1.0);
+    #elif SHADOW_TYPE == 1  // Soft
+      float shadow_detector_a = 1.0;
+      float shadow_black_a = 1.0;
+      vec4 shadow_color_a = vec4(1.0);
 
-            float shadow_detector_b = 1.0;
-            float shadow_black_b = 1.0;
-            vec4 shadow_color_b = vec4(1.0);
+      float shadow_detector_b = 1.0;
+      float shadow_black_b = 1.0;
+      vec4 shadow_color_b = vec4(1.0);
 
-            float shadow_detector_c = 1.0;
-            float shadow_black_c = 1.0;
-            vec4 shadow_color_c = vec4(1.0);
+      float alpha_complement;
 
-            float shadow_detector_d = 1.0;
-            float shadow_black_d = 1.0;
-            vec4 shadow_color_d = vec4(1.0);
+      #if AA_TYPE > 0
+        float dither = shifted_unit_dither(gl_FragCoord.xy);
+      #else
+        float dither = unit_dither(gl_FragCoord.xy);
+      #endif
 
-            float alpha_complement;
+      #if defined GBUFFER_ENTITIES
+		float subsurfaceOffset = 0.00002;
+	#else
+		float subsurfaceOffset = dot(normalM.xyz, normalize(shadowLightPosition)) < 0.0 ? 0.00001 : 0.00021;
+	#endif
 
-            float current_radius = dither;
-            dither *= 6.283185307179586;
-            float dither_2 = dither + 1.5707963267948966;
+    float new_z = the_shadow_pos.z - subsurfaceOffset - (0.0000 * dither);
 
-            vec2 offset = (vec2(cos(dither), sin(dither)) * current_radius * SHADOW_BLUR) / shadowMapResolution;
-            vec2 offset_2 = (vec2(cos(dither_2), sin(dither_2)) * (1.0 - current_radius) * SHADOW_BLUR) / shadowMapResolution;
+      float current_radius = dither;
+      dither *= 6.283185307;
 
-            float z_bias = dither * 0.00002;
+      vec2 offset = (vec2(cos(dither), sin(dither)) * current_radius * SHADOW_BLUR) / shadowMapResolution;
 
-            shadow_detector_a = shadow2D(shadowtex0, vec3(the_shadow_pos.xy + offset, the_shadow_pos.z - z_bias)).r;
-            shadow_detector_b = shadow2D(shadowtex0, vec3(the_shadow_pos.xy - offset, the_shadow_pos.z - z_bias)).r;
+      shadow_detector_a = shadow2D(shadowtex0, vec3(the_shadow_pos.xy + offset, new_z)).r;
+      shadow_detector_b = shadow2D(shadowtex0, vec3(the_shadow_pos.xy - offset, new_z)).r;
 
-            shadow_detector_c = shadow2D(shadowtex0, vec3(the_shadow_pos.xy + offset_2, the_shadow_pos.z - z_bias)).r;
-            shadow_detector_d = shadow2D(shadowtex0, vec3(the_shadow_pos.xy - offset_2, the_shadow_pos.z - z_bias)).r;
+      if (shadow_detector_a < 2.0) {
+        shadow_black_a = shadow2D(shadowtex1, vec3(the_shadow_pos.xy + offset, new_z)).r;
+        if (shadow_black_a != shadow_detector_a) {
+          shadow_color_a = texture2D(shadowcolor0, the_shadow_pos.xy + offset);
+          alpha_complement = 1.1 - shadow_color_a.a;
+          shadow_color_a.rgb = mix(shadow_color_a.rgb, vec3(1.0), alpha_complement);
+          shadow_color_a.rgb *= alpha_complement;
+          #if defined GBUFFER_TERRAIN || defined GBUFFER_ENTITIES || defined GBUFFER_HAND || defined GBUUFER_HAND_WATER
+          #ifdef UNDERWATER_CAUSTIC
+		 shadow_color_a.rgb += getCaustics(block_color) * CAUSTIC_STRENGTH;
+		#endif
+		#endif
+        }
+      }
+      
+      shadow_color_a *= shadow_black_a;
 
-            if (shadow_detector_a < 1.0) {
-                shadow_black_a = shadow2D(shadowtex1, vec3(the_shadow_pos.xy + offset, the_shadow_pos.z - z_bias)).r;
-                if (shadow_black_a != shadow_detector_a) {
-                shadow_color_a = texture2D(shadowcolor0, the_shadow_pos.xy + offset);
-                alpha_complement = 1.0 - shadow_color_a.a;
-                shadow_color_a.rgb = mix(shadow_color_a.rgb, vec3(1.0), alpha_complement);
-                shadow_color_a.rgb *= alpha_complement;
-                }
-            }
-            
-            shadow_color_a *= shadow_black_a;
+      if (shadow_detector_b < 1.0) {
+        shadow_black_b = shadow2D(shadowtex1, vec3(the_shadow_pos.xy - offset, new_z)).r;
+        if (shadow_black_b != shadow_detector_b) {
+          shadow_color_b = texture2D(shadowcolor0, the_shadow_pos.xy - offset);
+          alpha_complement = 1.1 - shadow_color_b.a;
+          shadow_color_b.rgb = mix(shadow_color_b.rgb, vec3(1.0), alpha_complement);
+          shadow_color_b.rgb *= alpha_complement;
+          #if defined GBUFFER_TERRAIN || defined GBUFFER_ENTITIES || defined GBUFFER_HAND || defined GBUUFER_HAND_WATER
+          #ifdef UNDERWATER_CAUSTIC
+		shadow_color_b.rgb += getCaustics(block_color) * CAUSTIC_STRENGTH;
+		#endif
+		#endif
+        }
+      }
+      
+      shadow_color_b *= shadow_black_b;
 
-            if (shadow_detector_b < 1.0) {
-                shadow_black_b = shadow2D(shadowtex1, vec3(the_shadow_pos.xy - offset, the_shadow_pos.z - z_bias)).r;
-                if (shadow_black_b != shadow_detector_b) {
-                shadow_color_b = texture2D(shadowcolor0, the_shadow_pos.xy - offset);
-                alpha_complement = 1.0 - shadow_color_b.a;
-                shadow_color_b.rgb = mix(shadow_color_b.rgb, vec3(1.0), alpha_complement);
-                shadow_color_b.rgb *= alpha_complement;
-                }
-            }
-            
-            shadow_color_b *= shadow_black_b;
+      shadow_detector_a = (shadow_detector_a + shadow_detector_b) * 0.5;
+      shadow_detector_a = clamp(shadow_detector_a * 2.0, 0.0, 1.0);
 
-            if (shadow_detector_c < 1.0) {
-                shadow_black_c = shadow2D(shadowtex1, vec3(the_shadow_pos.xy + offset_2, the_shadow_pos.z - z_bias)).r;
-                if (shadow_black_c != shadow_detector_c) {
-                shadow_color_c = texture2D(shadowcolor0, the_shadow_pos.xy + offset_2);
-                alpha_complement = 1.0 - shadow_color_c.a;
-                shadow_color_c.rgb = mix(shadow_color_c.rgb, vec3(1.0), alpha_complement);
-                shadow_color_c.rgb *= alpha_complement;
-                }
-            }
-            
-            shadow_color_c *= shadow_black_c;
+      shadow_color_a.rgb = (shadow_color_a.rgb + shadow_color_b.rgb) * 0.5;
+      shadow_color_a.rgb = mix(shadow_color_a.rgb, vec3(1.0), shadow_detector_a);
 
-            if (shadow_detector_d < 1.0) {
-                shadow_black_d = shadow2D(shadowtex1, vec3(the_shadow_pos.xy - offset_2, the_shadow_pos.z - z_bias)).r;
-                if (shadow_black_d != shadow_detector_d) {
-                shadow_color_d = texture2D(shadowcolor0, the_shadow_pos.xy - offset_2);
-                alpha_complement = 1.0 - shadow_color_d.a;
-                shadow_color_d.rgb = mix(shadow_color_d.rgb, vec3(1.0), alpha_complement);
-                shadow_color_d.rgb *= alpha_complement;
-                }
-            }
-            
-            shadow_color_d *= shadow_black_d;
+      return shadow_color_a.rgb;
+    #endif
 
-            shadow_detector_a = (shadow_detector_a + shadow_detector_b + shadow_detector_c + shadow_detector_d);
-            shadow_detector_a *= 0.25;
-
-            shadow_color_a.rgb = (shadow_color_a.rgb + shadow_color_b.rgb + shadow_color_c.rgb + shadow_color_d.rgb) * 0.25;
-            shadow_color_a.rgb = mix(shadow_color_a.rgb, vec3(1.0), shadow_detector_a);
-
-            return shadow_color_a.rgb;
-        #endif
-    }
-
+  }
+  
 #endif
--- common/solid_blocks_fragment.glsl
+++ common/solid_blocks_fragment.glsl
@@ -1,6 +1,8 @@
-#include "/lib/config.glsl"
+/* MakeUp - textured.fsh
+Fragment shader for translucent terrain rendering with enhanced gloss effect and customizable ground fog/smoke effect.
+*/
 
-// MAIN FUNCTION ------------------
+#include "/lib/config.glsl"
 
 #if defined THE_END
     #include "/lib/color_utils_end.glsl"
@@ -8,8 +10,12 @@
     #include "/lib/color_utils_nether.glsl"
 #endif
 
-/* Uniforms */
+/* Force enable shadow casting for testing */
+#define SHADOW_CASTING
 
+/* Uniforms */
+uniform sampler2D noisetex;
+uniform float frameTimeCounter;
 uniform float viewWidth;
 uniform float viewHeight;
 uniform int frameCounter;
@@ -21,10 +27,21 @@ uniform float light_mix;
 uniform float pixel_size_x;
 uniform float pixel_size_y;
 uniform sampler2D gaux4;
+uniform sampler2D depthtex1;
+uniform float near;
+uniform float far;
+uniform vec3 sunPosition;
+uniform vec3 moonPosition;
+uniform int worldTime;
+
+uniform sampler2DShadow shadowtex1;
+uniform sampler2DShadow shadowtex0;
+uniform sampler2D shadowcolor0;
 
 #if defined DISTANT_HORIZONS
     uniform float dhNearPlane;
-    uniform float far;
+    uniform float dhFarPlane;
+    uniform sampler2D dhDepthTex1;
 #endif
 
 #if defined GBUFFER_ENTITIES
@@ -37,10 +54,7 @@ uniform sampler2D gaux4;
 #endif
 
 #if defined SHADOW_CASTING
-    uniform sampler2DShadow shadowtex1;
     #if defined COLORED_SHADOW
-        uniform sampler2DShadow shadowtex0;
-        uniform sampler2D shadowcolor0;
     #endif
 #endif
 
@@ -51,21 +65,17 @@ uniform float blindness;
     uniform float darknessLightFactor;
 #endif
 
-#ifdef MATERIAL_GLOSS
-  // Don't remove
-#endif
-
 #if defined MATERIAL_GLOSS && !defined NETHER
-    uniform int worldTime;
-    uniform vec3 moonPosition;
-    uniform vec3 sunPosition;
     #if defined THE_END
         uniform mat4 gbufferModelView;
     #endif
 #endif
 
-/* Ins / Outs */
+/* Smoke control uniforms */
+uniform float smoke_mode = 2.0;       // 0.0 = выключен, 1.0 = локальный дым, 2.0 = полноэкранный дым
+uniform float smoke_fov_factor = 1.0; // Контроль области покрытия (0.0 - 1.0, где 1.0 = весь экран)
 
+/* Ins / Outs */
 varying vec2 texcoord;
 varying vec4 tint_color;
 varying float frog_adjust;
@@ -97,8 +107,21 @@ varying vec3 omni_light;
     varying float luma_power;
 #endif
 
-/* Utility functions */
+/* Constants */
+#define WATER_TURBULENCE 1.0
+#define SUN_REFLECTION 1
+#define SHADOW_TYPE 1
+#define SHADOW_BLUR 2.0
+#define COLORED_SHADOW
+#define REFLECTION_SLIDER 0
 
+/* Smoke effect parameters */
+#define SMOKE_DENSITY 0.3  // Плотность дыма (0.0 - 1.0)
+#define SMOKE_HEIGHT 0.5   // Максимальная высота дыма над землей (для локального режима)
+#define SMOKE_SPEED 0.1    // Скорость движения дыма
+#define SMOKE_SCALE 0.05   // Масштаб текстуры шума для дыма
+
+/* Utility functions */
 #if (defined SHADOW_CASTING && !defined NETHER) || defined DISTANT_HORIZONS
     #include "/lib/dither.glsl"
 #endif
@@ -113,6 +136,35 @@ varying vec3 omni_light;
     #include "/lib/material_gloss_fragment.glsl"
 #endif
 
+/* Smoke effect function */
+float calculate_smoke(vec3 fragpos, vec2 texcoord, float dither) {
+    if (smoke_mode < 0.5) return 0.0; // Эффект выключен
+
+    // Используем текстуру шума для создания органического эффекта
+    vec2 smoke_uv = texcoord + vec2(frameTimeCounter * SMOKE_SPEED, 0.0);
+    float noise = texture2D(noisetex, smoke_uv * SMOKE_SCALE).r;
+
+    // Контроль области покрытия (FoV)
+    vec2 centered_coord = texcoord * 2.0 - 1.0; // Нормализуем координаты от -1 до 1
+    float distance_from_center = length(centered_coord);
+    float fov_mask = smoothstep(smoke_fov_factor, smoke_fov_factor + 0.2, distance_from_center);
+    float fov_factor = 1.0 - fov_mask;
+
+    // Ограничиваем дым по высоте (только для локального режима)
+    float height_factor = 1.0;
+    if (smoke_mode < 1.5) { // Локальный режим
+        height_factor = clamp(1.0 - (fragpos.y / SMOKE_HEIGHT), 0.0, 1.0);
+    }
+
+    // Модулируем плотность дыма с учетом шума, высоты и FoV
+    float smoke_alpha = noise * height_factor * SMOKE_DENSITY * fov_factor;
+
+    // Добавляем случайность с помощью дитеринга
+    smoke_alpha *= (1.0 - dither * 0.2);
+
+    return clamp(smoke_alpha, 0.0, 1.0);
+}
+
 void main() {
     #if (defined SHADOW_CASTING && !defined NETHER) || defined DISTANT_HORIZONS
         #if AA_TYPE > 0 
@@ -121,86 +173,114 @@ void main() {
             float dither = r_dither(gl_FragCoord.xy);
         #endif
     #endif
+
     // Avoid render in DH transition
     #ifdef DISTANT_HORIZONS
         float t = far - dhNearPlane;
         float sup = t * TRANSITION_DH_SUP;
         float inf = t * TRANSITION_DH_INF;
         float umbral = (gl_FogFragCoord - (dhNearPlane + inf)) / (far - sup - inf - dhNearPlane);
-        if(umbral > dither) {
+        if (umbral > dither) {
             discard;
             return;
         }
     #endif
 
-    // Toma el color puro del bloque
-    #if defined GBUFFER_ENTITIES
-        #if BLACK_ENTITY_FIX == 1
-            vec4 block_color = texture2D(tex, texcoord);
-            if(block_color.a < 0.1 && entityId != 10101) {   // Black entities bug workaround
-                discard;
-            }
-            block_color *= tint_color;
+    // Проверяем, является ли текущий фрагмент водой
+    #ifdef GBUFFER_WATER
+        vec3 fragpos = sub_position3;
+        vec3 normal = flat_normal;
+        vec3 color = texture2D(tex, texcoord).rgb * tint_color.rgb;
+        vec3 sky_reflect = vec3(0.0);
+        vec3 reflected = reflect(normalize(fragpos), normal);
+        float fresnel = clamp(dot(normal, normalize(-fragpos)), 0.0, 1.0);
+        float visible_sky = 1.0 - rainStrength;
+        vec3 light_color = direct_light_color;
+
+        vec3 block_color = water_shader(
+            fragpos,
+            normal,
+            color,
+            sky_reflect,
+            reflected,
+            fresnel,
+            visible_sky,
+            dither,
+            light_color
+        );
+        vec4 final_color = vec4(block_color, tint_color.a * 0.8);
+    #else
+        // Обработка для непрозрачных объектов
+        #if defined GBUFFER_ENTITIES
+            #if BLACK_ENTITY_FIX == 1
+                vec4 block_color = texture2D(tex, texcoord);
+                if (block_color.a < 0.1 && entityId != 10101) {
+                    discard;
+                }
+                block_color *= tint_color;
+            #else
+                vec4 block_color = texture2D(tex, texcoord) * tint_color;
+            #endif
         #else
             vec4 block_color = texture2D(tex, texcoord) * tint_color;
         #endif
-    #else
-        vec4 block_color = texture2D(tex, texcoord) * tint_color;
-    #endif
 
         float block_luma = luma(block_color.rgb);
 
         vec3 final_candle_color = candle_color;
-    #if defined GBUFFER_TERRAIN || defined GBUFFER_HAND
-        if(emmisive_type > 0.5) {
-            final_candle_color *= block_luma * 1.5;
-        }
-    #endif
-
-    #ifdef GBUFFER_WEATHER
-        block_color.a *= .5;
-    #endif
-
-    #if defined GBUFFER_ENTITIES
-        // Thunderbolt render
-        if(entityId == 10101) {
-            block_color.a = 1.0;
-        }
-    #endif
-
-    #if defined SHADOW_CASTING && !defined NETHER
-        #if defined COLORED_SHADOW
-            vec3 shadow_c = get_colored_shadow(shadow_pos, dither);
-            shadow_c = mix(shadow_c, vec3(1.0), shadow_diffuse);
-        #else
-            float shadow_c = get_shadow(shadow_pos, dither);
-            shadow_c = mix(shadow_c, 1.0, shadow_diffuse);
+        #if defined GBUFFER_TERRAIN || defined GBUFFER_HAND
+            if (emmisive_type > 0.5) {
+                final_candle_color *= block_luma * 1.5;
+            }
         #endif
-    #else
-        float shadow_c = abs((light_mix * 2.0) - 1.0);
-    #endif
 
-    #if defined GBUFFER_BEACONBEAM
-        block_color.rgb *= 1.5;
-    #elif defined GBUFFER_ENTITY_GLOW
-        block_color.rgb =
-            clamp(vec3(luma(block_color.rgb)) * vec3(0.75, 0.75, 1.5), vec3(0.3), vec3(1.0));
-    #else
+        #ifdef GBUFFER_WEATHER
+            block_color.a *= .5;
+        #endif
+
+        #if defined GBUFFER_ENTITIES
+            if (entityId == 10101) {
+                block_color.a = 1.0;
+            }
+        #endif
+
+        // Определяем shadow_c как vec3 для поддержки цветных теней
+        vec3 shadow_c;
+        #if defined SHADOW_CASTING && !defined NETHER
+            #if defined COLORED_SHADOW
+                shadow_c = get_colored_shadow(shadow_pos, dither); // vec3 для цветных теней
+                shadow_c = mix(shadow_c, vec3(1.0), shadow_diffuse);
+            #else
+                float shadow_scalar = get_shadow(shadow_pos, dither); // float для обычных теней
+                shadow_c = vec3(mix(shadow_scalar, 1.0, shadow_diffuse)); // Преобразуем в vec3
+            #endif
+        #else
+            float shadow_scalar = abs((light_mix * 2.0) - 1.0);
+            shadow_c = vec3(shadow_scalar); // vec3 для консистентности
+        #endif
+
+        // Добавляем эффект дыма
+        float smoke_alpha = calculate_smoke(sub_position3, texcoord, dither);
+        vec3 smoke_color = vec3(0.7, 0.7, 0.8); // Цвет дыма (серо-голубой)
+        smoke_color *= shadow_c * (1.0 - rainStrength * 0.5); // Учитываем тени и дождь
+        block_color.rgb = mix(block_color.rgb, smoke_color, smoke_alpha);
+        block_color.a = max(block_color.a, smoke_alpha * 0.5); // Прозрачность дыма
+
         #if defined MATERIAL_GLOSS && !defined NETHER
-            float final_gloss_power = gloss_power;
+            float final_gloss_power = gloss_power * 1.5; // Увеличиваем силу глянца на 50%
             block_luma *= luma_factor;
 
-            if(luma_power < 0.0) {  // Metallic
+            if (luma_power < 0.0) {
                 final_gloss_power -= (block_luma * 73.334);
             } else {
                 block_luma = pow(block_luma, luma_power);
             }
 
-            float material_gloss_factor = material_gloss(reflect(normalize(sub_position3), flat_normal), lmcoord_alt, final_gloss_power, flat_normal) * gloss_factor;
+            float material_gloss_factor = material_gloss(reflect(normalize(sub_position3), flat_normal), lmcoord_alt, final_gloss_power, flat_normal) * gloss_factor * 1.2; // Усиливаем фактор глянца
 
-            float material = material_gloss_factor * block_luma;
+            float material = material_gloss_factor * block_luma * 1.3; // Увеличиваем вклад материала
             vec3 real_light = omni_light +
-                (shadow_c * ((direct_light_color * direct_light_strength) + (direct_light_color * material))) * (1.0 - (rainStrength * 0.75)) +
+                (shadow_c * ((direct_light_color * direct_light_strength * 1.2) + (direct_light_color * material))) * (1.0 - (rainStrength * 0.75)) +
                 final_candle_color;
         #else
             vec3 real_light = omni_light +
@@ -209,24 +289,23 @@ void main() {
         #endif
 
         block_color.rgb *= mix(real_light, vec3(1.0), nightVision * 0.125);
-        block_color.rgb *= mix(vec3(1.0, 1.0, 1.0), vec3(NV_COLOR_R, NV_COLOR_G, NV_COLOR_B), nightVision);
-    #endif
+        block_color.rgb *= mix(vec3(1.0), vec3(NV_COLOR_R, NV_COLOR_G, NV_COLOR_B), nightVision);
 
-    #if defined GBUFFER_ENTITIES
-        if(entityId == 10101) {
-            // Thunderbolt render
-            block_color = vec4(1.0, 1.0, 1.0, 0.5);
-        } else {
-            float entity_poderation = luma(real_light);  // Red damage bright ponderation
-            block_color.rgb = mix(block_color.rgb, entityColor.rgb, entityColor.a * entity_poderation * 3.0);
-        }
-    #endif
+        #if defined GBUFFER_ENTITIES
+            if (entityId == 10101) {
+                block_color = vec4(1.0, 1.0, 1.0, 0.5);
+            } else {
+                float entity_poderation = luma(real_light);
+                block_color.rgb = mix(block_color.rgb, entityColor.rgb, entityColor.a * entity_poderation * 3.0);
+            }
+        #endif
 
-    #if MC_VERSION < 11300 && defined GBUFFER_TEXTURED
-        block_color.rgb *= 1.5;
-    #endif
+        #if MC_VERSION < 11300 && defined GBUFFER_TEXTURED
+            block_color.rgb *= 1.5;
+        #endif
 
-        block_color.rgba = clamp(block_color, vec4(0.0), vec4(50.0));
+        vec4 final_color = clamp(block_color, vec4(0.0), vec4(50.0));
+    #endif
 
     #include "/src/finalcolor.glsl"
     #include "/src/writebuffers.glsl"
